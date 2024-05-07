6448b1cc55cdd50d567c740aca7994f5b91c2e99
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-05-07 12:14:38 +0700

    setUp-MUI

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,36 @@
 // import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
+
 
 function HomePage() {
+
+const navigate = useNavigate();
+
+const handleClick = () => {
+    let isValid = true
+   if (isValid) navigate('/random-path');
+   else navigate('/fftt');
+};
+
   return (
     <div>
       <h1>HomePage</h1>
+      
       <Link to='/login'>LoginPage</Link>
       <Link to='/profile'>ProfilePage</Link>
+      <Button
+       endIcon={<DeleteIcon/>}
+       variant="contained" 
+       disableElevation
+       color="error" 
+       size="small" 
+       onClick={handleClick}>
+        Go to 404
+        </Button>
     </div>
   )
 }
